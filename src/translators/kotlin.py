0aa7667bcbdd0779c5073fb0dedb2faa00c02940
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-11-27 10:56:58 +0200

    Create Kotlin files with good identation

--- src/translators/kotlin.py
+++ src/translators/kotlin.py
@@ -11,6 +11,7 @@ class KotlinTranslator(ASTVisitor):
     def __init__(self):
         self._children_res = []
         self.program = None
+        self.ident = 0
 
     @staticmethod
     def get_filename():
@@ -45,43 +46,56 @@ class KotlinTranslator(ASTVisitor):
         children = node.children()
         for c in children:
             c.accept(self)
-        self.program = '\n'.join(self.pop_children_res(children))
+        self.program = '\n\n'.join(self.pop_children_res(children))
 
     def visit_block(self, node):
         children = node.children()
         for c in children:
             c.accept(self)
         children_res = self.pop_children_res(children)
-        res = "{\n  " + "\n  ".join(children_res[:-1])
-        res += "\n  return " + children_res[-1] + "\n}"
+        res = "{\n" + "\n".join(children_res[:-1])
+        if children_res[:-1]:
+            res += "\n"
+        res += " " * self.ident  + "return " + \
+            children_res[-1][self.ident:] + "\n" + " " * (self.ident - 2) + "}"
         self._children_res.append(res)
 
     def visit_class_decl(self, node):
+        old_ident = self.ident
+        self.ident += 2
         children = node.children()
         for c in children:
             c.accept(self)
         children_res = self.pop_children_res(children)
         field_res = [children_res[i] for i, _ in enumerate(node.fields)]
         function_res = children_res[len(field_res):]
+        prefix = " " * old_ident
         if not field_res:
-            res = "class " + node.name
+            res = prefix + "class " + node.name
         else:
-            res = "class " + node.name + "(" + ", ".join(field_res) + ")"
+            res = prefix + "class " + node.name + "(" + ", ".join(
+                field_res) + ")"
         if node.superclasses:
             res += ": " + ", ".join(map(str, node.superclasses))
         if function_res:
-            res += "{\n  " + "\n".join(function_res) + "\n}"
+            res += " {\n" + "\n\n".join(
+                function_res) + "\n" + " " * old_ident + "}"
+        self.ident = old_ident
         self._children_res.append(res)
 
     def visit_var_decl(self, node):
+        old_ident = self.ident
+        prefix = " " * self.ident
+        self.ident = 0
         children = node.children()
         for c in children:
             c.accept(self)
         children_res = self.pop_children_res(children)
-        res = "val " + node.name
+        res = prefix + "val " + node.name
         if node.var_type is not None:
             res += ": " + node.var_type.name
         res += " = " + children_res[0]
+        self.ident = old_ident
         self._children_res.append(res)
 
     def visit_field_decl(self, node):
@@ -93,13 +107,16 @@ class KotlinTranslator(ASTVisitor):
         self._children_res.append(res)
 
     def visit_func_decl(self, node):
+        old_ident = self.ident
+        self.ident += 2
         children = node.children()
         for c in children:
             c.accept(self)
         children_res = self.pop_children_res(children)
         param_res = [children_res[i] for i, _ in enumerate(node.params)]
         body_res = children_res[-1]
-        res = "fun " + node.name + "(" + ", ".join(param_res) + ")"
+        prefix = " " * old_ident
+        res = prefix + "fun " + node.name + "(" + ", ".join(param_res) + ")"
         if node.ret_type:
             res += ": " + node.ret_type.name
             if isinstance(node.ret_type, kt.UnitType):
@@ -107,33 +124,38 @@ class KotlinTranslator(ASTVisitor):
                 # return type of the function is Unit.
                 body_res = "".join(body_res.rsplit("return", 1))
         res += " " + body_res
+        self.ident = old_ident
         self._children_res.append(res)
 
     def visit_integer_constant(self, node):
-        self._children_res.append(str(node.literal))
+        self._children_res.append(" " * self.ident + str(node.literal))
 
     def visit_real_constant(self, node):
-        self._children_res.append(str(node.literal))
+        self._children_res.append(" " * self.ident + str(node.literal))
 
     def visit_char_constant(self, node):
-        self._children_res.append("'{}'".format(node.literal))
+        self._children_res.append("{}'{}'".format(
+            " " * self.ident, node.literal))
 
     def visit_string_constant(self, node):
-        self._children_res.append('"{}"'.format(node.literal))
+        self._children_res.append('{}"{}"'.format(
+            " " * self.ident, node.literal))
 
     def visit_boolean_constant(self, node):
-        self._children_res.append(str(node.literal))
+        self._children_res.append(" " * self.ident + str(node.literal))
 
     def visit_variable(self, node):
-        self._children_res.append(node.name)
+        self._children_res.append(" " * self.ident + node.name)
 
     def visit_binary_op(self, node):
         children = node.children()
         for c in children:
             c.accept(self)
         children_res = self.pop_children_res(children)
-        self._children_res.append(
-            children_res[0] + " " + node.operator + " " + children_res[1])
+        res = "{}{} {} {}".format(
+            " " * self.ident, children_res[0], node.operator,
+            children_res[1])
+        self._children_res.append(res)
 
     def visit_logical_expr(self, node):
         self.visit_binary_op(node)
@@ -148,32 +170,43 @@ class KotlinTranslator(ASTVisitor):
         self.visit_binary_op(node)
 
     def visit_conditional(self, node):
+        old_ident = self.ident
+        self.ident += 2
         children = node.children()
         for c in children:
             c.accept(self)
         children_res = self.pop_children_res(children)
-        res = "if ({}) {} else {}".format(
-            children_res[0], children_res[1], children_res[2])
+        res = "{}if ({})\n{}\n{}else\n{}".format(
+            " " * old_ident, children_res[0][self.ident:], children_res[1],
+            " " * old_ident, children_res[2])
+        self.ident = old_ident
         self._children_res.append(res)
 
     def visit_new(self, node):
+        old_ident = self.ident
+        self.ident = 0
         children = node.children()
         for c in children:
             c.accept(self)
         children_res = self.pop_children_res(children)
+        self.ident = old_ident
         self._children_res.append(
-            node.class_name + "(" + ", ".join(children_res) + ")")
+            " " * self.ident + node.class_name + "(" + ", ".join(
+                children_res) + ")")
 
     def visit_field_access(self, node):
         raise NotImplementedError('visit_field_access() must be implemented')
 
     def visit_func_call(self, node):
+        old_ident = self.ident
+        self.ident = 0
         children = node.children()
         for c in children:
             c.accept(self)
+        self.ident = old_ident
         children_res = self.pop_children_res(children)
         self._children_res.append(
-            node.func + "(" + ", ".join(children_res) + ")")
+            " " * self.ident + node.func + "(" + ", ".join(children_res) + ")")
 
     def visit_assign(self, node):
         raise NotImplementedError('visit_assign() must be implemented')
