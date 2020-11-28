42f89e029ad7c6aea83cf201f7daed8c2a923d4e
StefanosChaliasos <stefanoshaliassos@gmail.com>
2020-11-28 20:17:40 +0200

    Add setup.py and tests structure
    To run the tests "python setup.py test"

---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+import glob
+from setuptools import setup, find_packages, Command
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(os.path.normpath(os.path.join(
+                here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute
+                    raise ValueError("%s is not a path inside %s" % (path,
+                                                                     here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
+
+
+setup(
+    name='checktypesystems',
+    version='0.0.1',
+    description='Check Type Systems',
+    python_requires='>=3.4, <4',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'mock'],
+    packages=['src'],
+    packages_dir={'src': 'src'},
+    #  entry_points={
+    #      'console_scripts': [
+    #          'check-type-systems=main',
+    #      ],
+    #  },
+    cmdclass={
+        'clean': CleanCommand,
+    },
+)
---
+++ tests/test_ir.py
@@ -0,0 +1,8 @@
+from src.ir.ast import *
+from src.ir.types import *
+from src.ir.kotlin_types import *
+
+
+def test_dummy():
+    assert 0 == 0,\
+        "Should be 0"
