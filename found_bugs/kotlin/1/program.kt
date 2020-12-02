01239c09739e6f60ff8786923d5ec097499b302b
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-12-02 22:56:38 +0200

    Add the first Kotlin bug found by our tool

---
+++ found_bugs/kotlin/1/program.kt
@@ -0,0 +1,27 @@
+fun hesitated(): Short {
+  fun fatah(): Boolean {
+    return false
+  }
+  val mission: Boolean = false
+  val stubbier: Boolean = false
+  val wined: Short = -59
+  val juridical: Short = if (false)
+  -91
+else
+  -23
+  val wonder: Short = if (if (false)
+    mission
+  else
+    stubbier)
+  if (false)
+    wined
+  else
+    if (true)
+      -20
+    else
+      -1
+else
+  juridical
+  val principe: Short = wonder
+  return principe
+}
