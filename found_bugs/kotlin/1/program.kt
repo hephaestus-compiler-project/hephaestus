f3e137390cb30505d8fa4d6c6d7ab31f333d1275
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-12-04 19:05:47 +0200

    Simplify example program

--- found_bugs/kotlin/1/program.kt
+++ found_bugs/kotlin/1/program.kt
@@ -1,27 +1,15 @@
 fun hesitated(): Short {
-  fun fatah(): Boolean {
-    return false
-  }
-  val mission: Boolean = false
-  val stubbier: Boolean = false
   val wined: Short = -59
-  val juridical: Short = if (false)
-  -91
-else
-  -23
-  val wonder: Short = if (if (false)
-    mission
-  else
-    stubbier)
-  if (false)
-    wined
-  else
+  val juridical: Short = -91
+  val wonder: Short =
     if (true)
-      -20
-    else
-      -1
-else
-  juridical
-  val principe: Short = wonder
-  return principe
+      if (false) wined
+      else
+        if (true) -20 else -1
+    else juridical
+  return wonder
+}
+
+fun main() {
+    hesitated()
 }
