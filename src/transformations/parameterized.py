d6bc214c94f4637260861b37893d129a2f061283
StefanosChaliasos <stefanoshaliassos@gmail.com>
2020-12-07 17:20:08 +0200

    Fix ParameterizedSubstitution

--- src/transformations/parameterized.py
+++ src/transformations/parameterized.py
@@ -5,12 +5,7 @@ from src import utils
 from src.ir import ast
 from src.ir import types
 from src.ir import kotlin_types as kt
-from src.ir.visitors import DefaultVisitor
-
-
-class Transformation(DefaultVisitor):
-    def __init__(self):
-        self.transform = False
+from src.transformations.base import Transformation
 
 
 def create_type_parameters(total):
@@ -21,7 +16,7 @@ def create_type_parameters(total):
     return [types.TypeParameter(c) for c in random_caps]
 
 
-def create_type_constructor(class_decl, type_parameters):
+def create_type_constructor_decl(class_decl, type_parameters):
     # TODO update body accordingly
     return ast.ClassDeclaration(
         class_decl.name, class_decl.superclasses,
@@ -32,12 +27,16 @@ def create_type_constructor(class_decl, type_parameters):
         type_parameters=type_parameters
     )
 
-class ParameterizedCreation(Transformation):
+
+class ParameterizedSubstitution(Transformation):
+    CORRECTNESS_PRESERVING = True
+    NAME = 'Parameterized Substitution'
 
     def __init__(self):
-        super(ParameterizedCreation, self).__init__()
+        super(ParameterizedSubstitution, self).__init__()
         self._old_class = None
-        self._type_constructor = None
+        self._old_class_decl = None
+        self._type_constructor_decl = None
         self._type_parameters = []
         self._parameterized_type = None
 
@@ -48,8 +47,14 @@ class ParameterizedCreation(Transformation):
         # TODO add constraints to select type_args
         type_args = [random.choice(kt.NonNothingTypes)
                      for _ in self._type_parameters]
-        return types.ParameterizedType(self._type_constructor.get_type(),
+        return types.ParameterizedType(self._type_constructor_decl.get_type(),
                                        type_args)
+
+    def get_candidates_classes(self):
+        """Get all simple classifiers declarations."""
+        return [d for d in self.program.declarations
+                if (isinstance(d, ast.ClassDeclaration) and
+                isinstance(d.get_type(), types.SimpleClassifier))]
 
     def result(self):
         return self.program
@@ -58,6 +63,7 @@ class ParameterizedCreation(Transformation):
         """Replace one class declaration with one type constructor and
         initialize type parameters.
         """
+        self.program = node
         classes = [d for d in node.declarations
                    if (isinstance(d, ast.ClassDeclaration) and
                        isinstance(d.get_type(), types.SimpleClassifier))]
@@ -66,58 +72,40 @@ class ParameterizedCreation(Transformation):
             return
         index = utils.random.integer(0, len(classes) - 1)
         class_decl = classes[index]
+        self._old_class_decl = class_decl
         self._old_class = class_decl.get_type()
         # TODO Maybe we can do something more specific here
         total_type_params = utils.random.integer(1, 1)
         self._type_parameters = create_type_parameters(total_type_params)
-        self._type_constructor = create_type_constructor(
+        self._type_constructor_decl = create_type_constructor_decl(
             class_decl, self._type_parameters)
         # TODO maybe we can do it more dynamically
         self._parameterized_type = self._create_parameterized_type()
-        decls = [d for d in node.declarations if d != class_decl]
-        self.program = ast.Program([self._type_constructor] + decls)
-        super(ParameterizedCreation, self).visit_program(self.program)
-
-    def visit_block(self, node):
-        # TODO
-        super(ParameterizedCreation, self).visit_block(node)
-        for e in node.body:
-            if not isinstance(e, ast.VariableDeclaration):
-                continue
+        return super(ParameterizedSubstitution, self).visit_program(self.program)
 
     def visit_class_decl(self, node):
-        # TODO
-        initial_namespace = self._namespace
-        self._namespace += (node.name,)
-        super(ParameterizedCreation, self).visit_class_decl(node)
-        for f in node.fields:
-            pass
-        self._namespace = initial_namespace
+        new_node = super(ParameterizedSubstitution, self).visit_class_decl(node)
+        if new_node == self._old_class_decl:
+            new_node = self._type_constructor_decl
+        #  for f in node.fields:
+            #  pass
+        return new_node
 
     def visit_param_decl(self, node):
         if node.param_type == self._old_class:
             node.param_type = self._parameterized_type
             if node.default:
                 raise NotImplementedError
-
-    def visit_field_decl(self, node):
-        # TODO
-        pass
+        return node
 
     def visit_var_decl(self, node):
-        super(ParameterizedCreation, self).visit_var_decl(node)
-        if node.var_type == self._old_class:
-            node.var_type = self._parameterized_type
+        new_node = super(ParameterizedSubstitution, self).visit_var_decl(node)
+        if new_node.var_type == self._old_class:
+            new_node.var_type = self._parameterized_type
+        return new_node
 
     def visit_func_decl(self, node):
-        # TODO
-        initial_namespace = self._namespace
-        self._namespace += (node.name,)
-        super(ParameterizedCreation, self).visit_func_decl(node)
-        for p in node.params:
-            pass
-        self._namespace = initial_namespace
-
-    def visit_variable(self, node):
-        # TODO
-        pass
+        new_node = super(ParameterizedSubstitution, self).visit_func_decl(node)
+        #  for p in node.params:
+            #  pass
+        return new_node
