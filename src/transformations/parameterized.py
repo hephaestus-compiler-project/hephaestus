380e664aae26b15fffb5960f8adf25e5cc8acfa1
StefanosChaliasos <stefanoshaliassos@gmail.com>
2020-12-08 18:23:04 +0200

    Add support for covariance and bounds

--- src/transformations/parameterized.py
+++ src/transformations/parameterized.py
@@ -8,16 +8,30 @@ from src.ir import kotlin_types as kt
 from src.transformations.base import Transformation
 
 
-def create_type_parameters(total):
-    # TODO Add variance and bound
+INVARIANT = types.TypeParameter.INVARIANT
+COVARIANT = types.TypeParameter.COVARIANT
+CONTRAVARIANT = types.TypeParameter.CONTRAVARIANT
+
+
+def get_type_params_names(total):
     random_caps = []
     for _ in range(0, total):
         random_caps += utils.random.caps(1, random_caps)
-    return [types.TypeParameter(c) for c in random_caps]
+    return random_caps
+
+
+def create_type_parameter(name: str, type_constraint: types.Type, out: bool):
+    """If out is True it means that we can create a covariant type parameter,
+    if it's not we can create a contravariant type parameter.
+    """
+    variance = random.choice([INVARIANT, COVARIANT if out else CONTRAVARIANT])
+    bound = None
+    if type_constraint is not None and random.random() < .5:
+        bound = random.choice(list(type_constraint.get_supertypes()))
+    return types.TypeParameter(name, variance, bound)
 
 
 def create_type_constructor_decl(class_decl, type_parameters):
-    # TODO update body accordingly
     return ast.ClassDeclaration(
         class_decl.name, class_decl.superclasses,
         class_type=class_decl.class_type,
@@ -29,17 +43,37 @@ def create_type_constructor_decl(class_decl, type_parameters):
 
 
 class ParameterizedSubstitution(Transformation):
+    """To create a ParameterizedType, we do the following steps:
+        1. Select a SimpleClassifier ClassDeclaration (visit_program)
+        2. Select how many TypeParameters we'll use (visit_program)
+        3. We choose where we will use the TypeParameters in the body of the
+           selected SimpleClassifier. Currently, we select only
+           FieldDeclarations, and FunctionDeclarations. Every time we choose
+           to replace a concrete type with a TypeParameter, we add the
+           corresponding constraints for the specific TypeParameter.
+           For example, suppose we replace the return type of a function.
+           In that case, the type argument for the specific TypeParameter must
+           have the same type as the return type we replaced, or we can specify
+           the TypeParameter to be covariant and the type argument to be a
+           supertype of the replaced return type.
+           (_use_type_parameter)
+        4. Create the TypeParameters based on the restrictions from step 3.
+           (_use_type_parameter)
+        5. Create the ParameterizedType with type arguments that respect the
+           constraints of TypeParameters.
+           (visit_class_decl)
+    """
     CORRECTNESS_PRESERVING = True
     NAME = 'Parameterized Substitution'
 
     def __init__(self, max_type_params=3):
         super(ParameterizedSubstitution, self).__init__()
+        self._max_type_params = max_type_params
         self._old_class = None
         self._old_class_decl = None
-        self._max_type_params = max_type_params
         self._type_constructor_decl = None
+        self._type_params_constraints = {} # Name -> (Type, covariant: bool)
         self._type_params = []
-        self._type_params_constraints = defaultdict(lambda: None)
         self._parameterized_type = None
         self._in_changed_type_decl = False
         self._in_override = False
@@ -48,15 +82,32 @@ class ParameterizedSubstitution(Transformation):
         self.program = None
 
     def _create_parameterized_type(self):
-        type_args = [random.choice(kt.NonNothingTypes)
-                     if self._type_params_constraints[tp] is None
-                     else self._type_params_constraints[tp]
-                     for tp in self._type_params]
+        type_args = []
+        for tp in self._type_params:
+            constraint = self._type_params_constraints[tp.name]
+            if constraint is None:
+                possible_types = kt.NonNothingTypes
+            else:
+                constraint = constraint[0]
+                if tp.variance == INVARIANT:
+                    type_args.append(constraint)
+                    continue
+                possible_types = []
+                if tp.variance == CONTRAVARIANT:
+                    possible_types = list(constraint.get_supertypes())
+                if tp.variance == COVARIANT:
+                    possible_types = self.find_subtypes(constraint)
+                    possible_types += [bt for bt in kt.NonNothingTypes
+                                       if bt.is_subtype(constraint)]
+                if tp.bound:
+                    possible_types = [t for t in possible_types
+                                      if t.is_subtype(tp.bound)]
+            type_args.append(random.choice(possible_types))
         return types.ParameterizedType(self._type_constructor_decl.get_type(),
                                        type_args)
 
     def get_candidates_classes(self):
-        """Get all simple classifiers declarations."""
+        """Get all simple classifier declarations."""
         return [d for d in self.program.declarations
                 if (isinstance(d, ast.ClassDeclaration) and
                 isinstance(d.get_type(), types.SimpleClassifier))]
@@ -81,38 +132,50 @@ class ParameterizedSubstitution(Transformation):
             ## There are not user-defined simple classifier declarations.
             return
         index = utils.random.integer(0, len(classes) - 1)
+        index = 0
         class_decl = classes[index]
         self._old_class_decl = class_decl
         self._old_class = class_decl.get_type()
-        # TODO Maybe we can do something more specific here
         total_type_params = utils.random.integer(1, self._max_type_params)
-        self._type_params = create_type_parameters(total_type_params)
-        self._type_constructor_decl = create_type_constructor_decl(
-            class_decl, self._type_params)
+        self._type_params_constraints = {
+            name: None for name in get_type_params_names(total_type_params)}
         return super(ParameterizedSubstitution, self).visit_program(self.program)
 
     def visit_class_decl(self, node):
         if node == self._old_class_decl:
             self._in_changed_type_decl = True
         new_node = super(ParameterizedSubstitution, self).visit_class_decl(node)
-        new_node = self._type_constructor_decl
-        if self._in_changed_type_decl:
+        if self._in_changed_type_decl and node == self._old_class_decl:
+            # Initiate unused type_params
+            self._type_params.extend([
+                create_type_parameter(tp_name, None, False)
+                for tp_name, constraint in self._type_params_constraints.items()
+                if constraint is None
+            ])
+            self._type_constructor_decl = create_type_constructor_decl(
+                self._old_class_decl, self._type_params
+            )
+            new_node = self._type_constructor_decl
             self._parameterized_type = self._create_parameterized_type()
-            self._in_changed_type_decl = False
+        self._in_changed_type_decl = False
         return new_node
 
-    def _use_type_parameter(self, t):
+    def _use_type_parameter(self, t, covariant=False):
         """Change concrete type with type parameter and add the corresponding
         constraints to type parameters.
         """
         if self._in_changed_type_decl:
             if self._in_override:
                 return t
-            # TODO Add randomness
-            for tp in self._type_params:
-                if self._type_params_constraints[tp] is None:
-                    self._type_params_constraints[tp] = t
-                    return tp
+            # We can increase the probability based on already used type_params
+            if random.random() < .5:
+                return t
+            for tp_name, constraints in self._type_params_constraints.items():
+                if constraints is None:
+                    self._type_params_constraints[tp_name] = (t, covariant)
+                    type_param = create_type_parameter(tp_name, t, covariant)
+                    self._type_params.append(type_param)
+                    return type_param
         return t
 
     def visit_super_instantiation(self, node):
@@ -121,11 +184,10 @@ class ParameterizedSubstitution(Transformation):
 
     def visit_new(self, node):
         new_node = super(ParameterizedSubstitution, self).visit_new(node)
-        # TODO update args?
         return self.update_type(new_node, 'class_type')
 
     def visit_field_decl(self, node):
-        node.field_type = self._use_type_parameter(node.field_type)
+        node.field_type = self._use_type_parameter(node.field_type, True)
         return self.update_type(node, 'field_type')
 
     def visit_param_decl(self, node):
@@ -140,7 +202,7 @@ class ParameterizedSubstitution(Transformation):
         if node.override:
             self._in_override = True
         if node.ret_type != kt.Unit:
-            node.ret_type = self._use_type_parameter(node.ret_type)
+            node.ret_type = self._use_type_parameter(node.ret_type, True)
         new_node = super(ParameterizedSubstitution, self).visit_func_decl(node)
         new_node = self.update_type(new_node, 'ret_type')
         self._in_override = False
