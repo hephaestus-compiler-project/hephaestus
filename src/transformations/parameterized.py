4ca006430926d11d370b3da7316541ada59a44e1
StefanosChaliasos <stefanoshaliassos@gmail.com>
2020-12-08 12:28:26 +0200

    Use type parameters in type constructor's declegrations (wip)

--- src/transformations/parameterized.py
+++ src/transformations/parameterized.py
@@ -32,21 +32,25 @@ class ParameterizedSubstitution(Transformation):
     CORRECTNESS_PRESERVING = True
     NAME = 'Parameterized Substitution'
 
-    def __init__(self):
+    def __init__(self, max_type_params=3):
         super(ParameterizedSubstitution, self).__init__()
         self._old_class = None
         self._old_class_decl = None
+        self._max_type_params = max_type_params
         self._type_constructor_decl = None
-        self._type_parameters = []
+        self._type_params = []
+        self._type_params_constraints = defaultdict(lambda: None)
         self._parameterized_type = None
+        self._in_changed_type_decl = False
 
         self._namespace = ('global',)
         self.program = None
 
     def _create_parameterized_type(self):
-        # TODO add constraints to select type_args
         type_args = [random.choice(kt.NonNothingTypes)
-                     for _ in self._type_parameters]
+                     if self._type_params_constraints[tp] is None
+                     else self._type_params_constraints[tp]
+                     for tp in self._type_params]
         return types.ParameterizedType(self._type_constructor_decl.get_type(),
                                        type_args)
 
@@ -59,16 +63,9 @@ class ParameterizedSubstitution(Transformation):
     def result(self):
         return self.program
 
-    def update_type(self, node, attr, is_decl=False):
-        old_type = self._old_class_decl if is_decl else self._old_class
-        new_type = self._type_constructor_decl if is_decl \
-                   else self._parameterized_type
-        if attr is None:
-            if node == old_type:
-                return new_type
-            return node
-        if getattr(node, attr) == old_type:
-            setattr(node, attr, new_type)
+    def update_type(self, node, attr):
+        if getattr(node, attr) == self._old_class:
+            setattr(node, attr, self._parameterized_type)
         return node
 
     def visit_program(self, node):
@@ -83,24 +80,38 @@ class ParameterizedSubstitution(Transformation):
             ## There are not user-defined simple classifier declarations.
             return
         index = utils.random.integer(0, len(classes) - 1)
+        index = 0
         class_decl = classes[index]
         self._old_class_decl = class_decl
         self._old_class = class_decl.get_type()
         # TODO Maybe we can do something more specific here
-        total_type_params = utils.random.integer(1, 1)
-        self._type_parameters = create_type_parameters(total_type_params)
+        total_type_params = utils.random.integer(1, self._max_type_params)
+        self._type_params = create_type_parameters(total_type_params)
         self._type_constructor_decl = create_type_constructor_decl(
-            class_decl, self._type_parameters)
-        # TODO maybe we can do it more dynamically
-        self._parameterized_type = self._create_parameterized_type()
+            class_decl, self._type_params)
         return super(ParameterizedSubstitution, self).visit_program(self.program)
 
     def visit_class_decl(self, node):
+        if node == self._old_class_decl:
+            self._in_changed_type_decl = True
         new_node = super(ParameterizedSubstitution, self).visit_class_decl(node)
-        new_node = self.update_type(new_node, attr=None, is_decl=True)
-        #  for f in node.fields:
-            #  pass
+        new_node = self._type_constructor_decl
+        if self._in_changed_type_decl:
+            self._parameterized_type = self._create_parameterized_type()
+            self._in_changed_type_decl = False
         return new_node
+
+    def _use_type_parameter(self, t):
+        """Change concrete type with type parameter and add the corresponding
+        constraints to type parameters.
+        """
+        if self._in_changed_type_decl:
+            # TODO Add randomness
+            for tp in self._type_params:
+                if self._type_params_constraints[tp] is None:
+                    self._type_params_constraints[tp] = t
+                    return tp
+        return t
 
     def visit_super_instantiation(self, node):
         new_node = super(ParameterizedSubstitution, self).visit_super_instantiation(node)
@@ -112,6 +123,7 @@ class ParameterizedSubstitution(Transformation):
         return self.update_type(new_node, 'class_type')
 
     def visit_param_decl(self, node):
+        node.param_type = self._use_type_parameter(node.param_type)
         return self.update_type(node, 'param_type')
 
     def visit_var_decl(self, node):
@@ -119,8 +131,8 @@ class ParameterizedSubstitution(Transformation):
         return self.update_type(new_node, 'var_type')
 
     def visit_func_decl(self, node):
+        if node.ret_type != kt.Unit:
+            node.ret_type = self._use_type_parameter(node.ret_type)
         new_node = super(ParameterizedSubstitution, self).visit_func_decl(node)
         new_node = self.update_type(new_node, 'ret_type')
-        #  for p in node.params:
-            #  pass
         return new_node
