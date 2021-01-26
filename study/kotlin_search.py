d7d1397ea915272ba859db64c092cf238a32338f
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-01-26 19:13:12 +0200

    Add script for Kotlin bugs

---
+++ study/kotlin_search.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+data = []
+first = True
+skip = 0
+top = 2500
+temp_data = []
+while len(temp_data) > 0 or first:
+    first = False
+    print('fetching ' + str(skip))
+    response = requests.get('https://youtrack.jetbrains.com/api/issues?query=project:%20Kotlin%20State:%20Fixed%20Type:%20Bug&fields=$type,created,customFields($type,id,name,projectCustomField($type,field($type,fieldType($type,id),id,localizedName,name),id),value($type,id,name)),description,id,idReadable,numberInProject,resolved,summary,updated,tags,project&$skip='+ str(skip) + '&$top=2500')
+    temp_data = response.json()
+    data.extend(temp_data)
+    skip += top
+
+
+print('writing data')
+with open('kotlin.json', 'w') as f:
+    f.write(json.dumps(data))
