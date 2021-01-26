478e687a8a0d3cf302f13f563211e7d5f5a2b861
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-01-26 20:23:15 +0200

    Add reporter

--- study/kotlin_search.py
+++ study/kotlin_search.py
@@ -10,7 +10,7 @@ temp_data = []
 while len(temp_data) > 0 or first:
     first = False
     print('fetching ' + str(skip))
-    response = requests.get('https://youtrack.jetbrains.com/api/issues?query=project:%20Kotlin%20State:%20Fixed%20Type:%20Bug&fields=$type,created,customFields($type,id,name,projectCustomField($type,field($type,fieldType($type,id),id,localizedName,name),id),value($type,id,name)),description,id,idReadable,numberInProject,resolved,summary,updated,tags,project&$skip='+ str(skip) + '&$top=2500')
+    response = requests.get('https://youtrack.jetbrains.com/api/issues?query=project:%20Kotlin%20State:%20Fixed%20Type:%20Bug&fields=$type,created,customFields($type,id,name,projectCustomField($type,field($type,fieldType($type,id),id,localizedName,name),id),value($type,id,name)),description,id,idReadable,numberInProject,resolved,summary,updated,tags,project,reporter($type,id,login,name,ringId)&$skip='+ str(skip) + '&$top=2500')
     temp_data = response.json()
     data.extend(temp_data)
     skip += top
