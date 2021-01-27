b575c15465431d21dc2b27c9834a1e88790a80d4
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-01-27 17:31:32 +0200

    Handle variable as receiver

--- src/analysis/call_analysis.py
+++ src/analysis/call_analysis.py
@@ -2,10 +2,14 @@
 from typing import Tuple, NamedTuple, List
 from collections import defaultdict
 
+import src.graph_utils as gu
+import src.ir.type_utils as tu
+import src.utils as ut
 from src.ir import ast
+from src.ir import types as tp
 from src.ir.visitors import DefaultVisitor
 from src.transformations.base import change_namespace
-from src.analysis.use_analysis import UseAnalysis
+from src.analysis.use_analysis import UseAnalysis, GNode, get_decl
 
 
 class CNode(NamedTuple):
@@ -51,14 +55,53 @@ class CallAnalysis(DefaultVisitor):
     def result(self):
         return self._call_graph, self._calls
 
-    def _get_func_namespace(self, func: str, receiver: str = None):
+    def _get_func_namespace(self, func: str, receiver: ast.Expr = None):
+        def get_filtered_or_all(namespace, namespaces):
+            res = []
+            for ns in namespaces:
+                if ut.prefix_lst(namespace, ns):
+                    res.append(ns)
+            if len(res) == 0:
+                return namespaces
+            return res
+
         funcs = self.program.context.get_namespaces_decls(
             self._namespace, func, 'funcs')
+
+        # Not a user defined function
         if len(funcs) == 0:
             return None
-        # TODO handle receiver
-        namespace, _ = list(funcs)[0]
-        return namespace
+
+        if len(funcs) == 1:
+            namespace, _ = list(funcs)[0]
+            return [namespace]
+
+        # Handle receiver
+        all_namespaces = [func[0] for func in funcs]
+        if isinstance(receiver, ast.Variable):
+            # Find the type of the source of the variable
+            gnode = GNode(self._namespace, receiver.name)
+            sources = gu.find_sources(self._use_graph, gnode)
+            if len(sources) > 1:
+                return all_namespaces
+            source = sources[0]
+            _, source_decl = get_decl(self.program.context,
+                                      self._namespace, source.name)
+            source_type = tu.get_var_decl_type(source_decl)
+            if isinstance(source_type, tp.Builtin):
+                return all_namespaces
+            # Get the namespace of source_type
+            # It is not possible to have multiple classes with the same name
+            namespace, _ = list(self.program.context.get_namespaces_decls(
+                self._namespace, source_type.name, 'classes'))[0]
+            return get_filtered_or_all(namespace, all_namespaces)
+        if isinstance(receiver, ast.New):
+            pass
+        if isinstance(receiver, ast.FunctionCall):
+            pass
+        if isinstance(receiver, ast.FieldAccess):
+            pass
+        return all_namespaces
 
     def _compute_use_graph(self, node):
         if len(self._namespace) == 2:
@@ -82,5 +125,6 @@ class CallAnalysis(DefaultVisitor):
         super().visit_func_call(node)
         callee_ns = self._get_func_namespace(node.func, node.receiver)
         if callee_ns:
-            self._call_graph[CNode(self._namespace)].add(CNode(callee_ns))
-            self._calls[CNode(callee_ns)].add(node)
+            for ns in callee_ns:
+                self._call_graph[CNode(self._namespace)].add(CNode(ns))
+                self._calls[CNode(ns)].add(node)
