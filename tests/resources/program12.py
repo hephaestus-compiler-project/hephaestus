f5506e62a261eb6c43379f604562c039e18aec2a
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-01-28 10:20:15 +0200

    Add one more program example

---
+++ tests/resources/program12.py
@@ -0,0 +1,90 @@
+from src.ir import ast, kotlin_types as kt
+from src.ir.context import Context
+
+# open class First {
+#     open fun foo() {}
+# }
+#
+# class Second(): First() {
+#     override fun foo() {}
+# }
+#
+# fun foo(x: First): First {
+#     return x
+# }
+#
+# fun bar() {
+#     B().foo()
+#     foo(A()).foo()
+# }
+
+first_foo = ast.FunctionDeclaration(
+    "foo",
+    params=[],
+    ret_type=kt.Unit,
+    body=ast.Block([]),
+    func_type=ast.FunctionDeclaration.CLASS_METHOD,
+    is_final=False
+)
+cls_first = ast.ClassDeclaration(
+    "First",
+    [],
+    ast.ClassDeclaration.REGULAR,
+    fields=[],
+    functions=[first_foo],
+    is_final=False
+)
+
+second_foo = ast.FunctionDeclaration(
+    "foo",
+    params=[],
+    ret_type=kt.Unit,
+    body=ast.Block([]),
+    func_type=ast.FunctionDeclaration.CLASS_METHOD,
+    is_final=True,
+    override=True
+)
+cls_second = ast.ClassDeclaration(
+    "Second",
+    [ast.SuperClassInstantiation(cls_first.get_type(), [])],
+    ast.ClassDeclaration.REGULAR,
+    fields=[],
+    functions=[second_foo]
+)
+
+foo_x = ast.ParameterDeclaration("x", cls_first.get_type())
+fun_foo = ast.FunctionDeclaration(
+    "foo",
+    params=[foo_x],
+    ret_type=cls_first.get_type(),
+    body=ast.Variable("x"),
+    func_type=ast.FunctionDeclaration.FUNCTION
+)
+
+
+fun_bar = ast.FunctionDeclaration(
+    "bar",
+    params=[],
+    ret_type=kt.Unit,
+    body=ast.Block([
+        ast.FunctionCall(
+            "foo", [],
+            ast.New(cls_second.get_type(), [])
+        ),
+        ast.FunctionCall(
+            "foo", [],
+            ast.FunctionCall("foo", [ast.New(cls_first.get_type(), [])]),
+        )
+    ]),
+    func_type=ast.FunctionDeclaration.FUNCTION
+)
+
+ctx = Context()
+ctx.add_class(ast.GLOBAL_NAMESPACE, "First", cls_first)
+ctx.add_func(ast.GLOBAL_NAMESPACE + ("First",), first_foo.name, first_foo)
+ctx.add_class(ast.GLOBAL_NAMESPACE, "Second", cls_second)
+ctx.add_func(ast.GLOBAL_NAMESPACE + ("Second",), second_foo.name, second_foo)
+ctx.add_func(ast.GLOBAL_NAMESPACE, "foo", fun_foo)
+ctx.add_var(ast.GLOBAL_NAMESPACE + ("foo",), foo_x.name, foo_x)
+ctx.add_func(ast.GLOBAL_NAMESPACE, "bar", fun_bar)
+program = ast.Program(ctx)
