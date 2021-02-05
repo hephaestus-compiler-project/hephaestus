fd1bff37a1aa959734b0bb5ef58ffc5fb350e3b8
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-02-05 19:58:44 +0200

    Fix visit_is

--- src/translators/groovy.py
+++ src/translators/groovy.py
@@ -412,7 +412,7 @@ class GroovyTranslator(ASTVisitor):
         res = "{}{} {} {}".format(
             " " * old_ident,
             children_res[0],
-            "!" if node.is_not else "" + "instanceof",
+            "!" if node.operator.is_not else "" + "instanceof",
             node.rexpr.get_name())
         self.ident = old_ident
         return res
