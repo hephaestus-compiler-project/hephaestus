ec40f4c0db4413514fa7ed43ca56a7bd073d0d75
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2021-02-23 17:29:55 +0200

    Add utils for printing stats

---
+++ study/categorization/stats.py
@@ -0,0 +1,4 @@
+from kotlin import kotlin_iter1
+from utils import print_stats
+
+print_stats(kotlin_iter1)
---
+++ study/categorization/utils.py
@@ -0,0 +1,61 @@
+import json
+from collections import defaultdict
+
+def print_characteristics():
+    def get_name(characteristic):
+        if characteristic.is_category:
+            sub_categories = [
+                s.name for s in SpecificCharacteristic.__subclasses__()
+                if s.specific_category == characteristic
+            ]
+            return characteristic.name + "(" + ", ".join(sub_categories) + ")"
+        return characteristic.name
+    print("======Characteristic======")
+    chars = {
+        g.name: [get_name(s) for s in SpecificCharacteristic.__subclasses__() 
+        if s.general_characteristic == g] 
+        for g in GeneralCharacteristic.__subclasses__()
+    }
+    for g, s in chars.items():
+        print(g)
+        if s:
+            print("\t" + "\n\t".join(s))
+    print("==========================")
+
+def print_stats(bugs):
+    print("======Statistics======")
+    stats = {
+        "Bugs": defaultdict(lambda: 0),
+        "Characteristics": {
+            "Categories": defaultdict(
+                lambda: {"total": 0, "subcategories": defaultdict(lambda: 0)}),
+            "Types": defaultdict(lambda: 0),
+            "Commons": {"True": 0, "False": 0}},
+        "Correctness": {"Correct": 0, "Incorrect": 0},
+        "Symptoms": defaultdict(lambda: 0),
+        "Root Causes": defaultdict(lambda: 0),
+        "Categories": defaultdict(lambda: 0)
+    }
+    for b in bugs:
+        stats['Bugs'][b.language] += 1
+        for c in b.characteristics:
+            cat = stats["Characteristics"]["Categories"][c.category.name]
+            cat["total"] += 1
+            if c.characteristic_type:
+                cat["subcategories"][c.characteristic_type.name] += 1
+            stats["Characteristics"]["Commons"][str(c.is_common)] += 1
+            if c.characteristic_type:
+                stats["Characteristics"]["Types"][c.characteristic_type.name] += 1
+        #for c in b.characteristics:
+        #    stats["Characteristics"][c.name]["total"] += 1
+        #    for s in c.specific_characteristics:
+        #        stats["Characteristics"][c.name]["specific"][s.name] += 1
+        if b.test_case_correct:
+            stats["Correctness"]["Correct"] += 1
+        else:
+            stats["Correctness"]["Incorrect"] += 1
+        stats["Symptoms"][b.symptom.name] += 1
+        stats["Categories"][b.category.name] += 1
+        stats["Root Causes"][b.root_cause.name] += 1
+    print(json.dumps(stats, indent=4))
+    print("======================")
