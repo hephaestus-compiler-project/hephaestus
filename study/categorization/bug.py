7af4cdd7f685a39ce9f4611f1adad0a6fc1b6ac1
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2021-02-25 11:48:20 +0200

    Add lines param to Bug that represent LOC of test case

--- study/categorization/bug.py
+++ study/categorization/bug.py
@@ -2,22 +2,24 @@ class Bug:
     language = ""
 
     def __init__(self, bug_id, characteristics, test_case_correct, symptom,
-                 root_cause, category):
+                 root_cause, category, lines=0):
         self.bug_id = bug_id
         self.characteristics = characteristics
         self.test_case_correct = test_case_correct
         self.symptom = symptom
         self.root_cause = root_cause
         self.category = category
+        self.lines = 0 # test case loc
 
     def __repr__(self):
-        return "Bug: {} (\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n)".format(
+        return "Bug: {} (\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n)".format(
             self.bug_id,
             "Characteristics: \n\t\t" + "\n\t\t".join([str(c) for c in self.characteristics]),
             "Test Case Correct: " + str(self.test_case_correct),
             "Symptom: " + str(self.symptom),
             "Root Cause: " + str(self.root_cause),
-            "Category: " + str(self.category)
+            "Category: " + str(self.category),
+            "Test case loc: " + str(self.lines)
         )
 
     def __str__(self):
