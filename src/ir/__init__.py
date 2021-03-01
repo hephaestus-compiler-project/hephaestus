4a2edcb5b55263165e378ea41fbe166fe430e8c1
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-03-01 17:30:41 +0200

    Add Java types and factory

--- src/ir/__init__.py
+++ src/ir/__init__.py
@@ -1,7 +1,9 @@
 from src.ir.kotlin_types import KotlinBuiltinFactory
 from src.ir.groovy_types import GroovyBuiltinFactory
+from src.ir.java_types import JavaBuiltinFactory
 
 BUILTIN_FACTORIES = {
     "kotlin": KotlinBuiltinFactory(),
-    "groovy": GroovyBuiltinFactory()
+    "groovy": GroovyBuiltinFactory(),
+    "java": JavaBuiltinFactory()
 }
