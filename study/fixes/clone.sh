7c46b770a31a7199f6c34f823c16fc0efa210757
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-03-01 20:08:40 +0200

    Add scripts to get stats for fixes

---
+++ study/fixes/clone.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+git clone https://github.com/JetBrains/kotlin
+git clone https://github.com/apache/groovy
+git clone https://github.com/lampepfl/dotty
+git clone https://github.com/scala/scala
+git clone https://github.com/openjdk/valhalla
+git clone https://github.com/openjdk/jdk16
+git clone https://github.com/openjdk/jdk
