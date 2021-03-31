e26f7fc33134b1ad649fed2f2462c4aa1146e1a1
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-03-31 13:37:18 +0300

    Update plotting of patterns figure

--- study/categorization/patterns_fig.py
+++ study/categorization/patterns_fig.py
@@ -6,7 +6,11 @@ import seaborn as sns
 import pandas as pd
 
 import categories as ct
-from utils import stats
+
+import kotlin as kt
+import java as jv
+import scala as sc
+import groovy as gv
 
 
 def map_category_name(category_name):
@@ -23,24 +27,23 @@ def map_category_name(category_name):
     elif category_name in [
             ct.Resolution().name,
             ct.Environment().name]:
-        return "Resolution Bugs"
+        return "Resolution & Environment Bugs"
     else:
         return category_name
 
 
-def construct_dataframe(data):
-    new_data = defaultdict(lambda: 0)
-    for key, value in data.items():
-        new_key = map_category_name(key)
-        new_data[new_key] += int(value)
-
+def construct_dataframe(bugs):
+    data = defaultdict(lambda: 0)
+    for bug in bugs:
+        data[(bug.language, map_category_name(bug.category.name))] += 1
     framedata = []
-    for key, value in new_data.items():
+    for (lang, category), value in data.items():
         framedata.append({
-            'Type': key,
-            'Number of bugs': int(value),
+            "Pattern": category,
+            "Language": lang,
+            "Number of bugs": value
         })
-    return pd.DataFrame(framedata)
+    return pd.DataFrame(framedata), data
 
 
 plt.style.use('ggplot')
@@ -53,15 +56,37 @@ plt.rcParams['font.serif'] = 'DejaVu Sans'
 plt.rcParams['font.monospace'] = 'Inconsolata Medium'
 plt.rcParams['axes.labelweight'] = 'bold'
 
-dataframe = construct_dataframe(stats['Categories']).sort_values(
-    'Number of bugs', ascending=True)
-ax = dataframe.plot.barh(width=0.3, x='Type', color='grey')
-for p in ax.patches:
-    ax.annotate("{} / 160".format(p.get_width()),
+
+bugs = jv.java_iter1 + jv.java_iter2 + jv.java_iter3 + \
+    sc.scala_iter1 + sc.scala_iter2 + sc.scala_iter3 + \
+    kt.kotlin_iter1 + kt.kotlin_iter2 + kt.kotlin_iter3 + \
+    gv.groovy_iter1 + gv.groovy_iter2 + gv.groovy_iter3
+df, data = construct_dataframe(bugs)
+df = df.groupby(['Language', 'Pattern'])['Number of bugs'].sum().unstack(
+    'Language')
+categories = [
+    ct.ErrorReporting().name,
+    ct.Transformation().name,
+    'Semantic Checking Bugs',
+    'Resolution & Environment Bugs',
+    'Type-related Bugs',
+]
+df = df.reindex(categories)
+
+ax = df.plot.barh(width=0.3,
+                  color=['#f6cb7d', '#873e23', '#e28743', '#8A0528'],
+                  stacked=True)
+
+sums = []
+for c in categories:
+    v = sum(data[(lang, c)] for lang in ['Groovy', 'Java', 'Kotlin', 'Scala'])
+    sums.append(v)
+
+for i, p in enumerate(ax.patches[15:]):
+    ax.annotate("{} / 240".format(int(sums[i])),
                 (p.get_x() + p.get_width(), p.get_y()),
                 xytext=(5, 10), textcoords='offset points')
 ax.set_ylabel('')
 patches, labels = ax.get_legend_handles_labels()
-ax.legend(patches, ('Number of bugs',), loc='lower right')
 plt.savefig("patterns.pdf", format='pdf', bbox_inches='tight',
             pad_inches=0)
