5d85f65bbc0034f7750421cea77ed6b8fa1d3128
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-03-31 11:49:20 +0300

    Update figure for symptoms

--- study/categorization/symptoms_fig.py
+++ study/categorization/symptoms_fig.py
@@ -1,19 +1,29 @@
 #! /usr/bin/env python3
+from collections import defaultdict
+
 import matplotlib.pylab as plt
 import seaborn as sns
 import pandas as pd
 
-from utils import stats
+import kotlin as kt
+import java as jv
+import scala as sc
+import groovy as gv
 
 
-def construct_dataframe(data):
+def construct_dataframe(bugs):
+
+    data = defaultdict(lambda: 0)
+    for bug in bugs:
+        data[(bug.language, bug.symptom.name)] += 1
     framedata = []
-    for key, value in data.items():
+    for (lang, symptom), value in data.items():
         framedata.append({
-            'Symptom': key,
-            'Number of bugs': int(value),
+            "Symptom": symptom,
+            "Language": lang,
+            "Number of bugs": value
         })
-    return pd.DataFrame(framedata)
+    return pd.DataFrame(framedata), data
 
 
 plt.style.use('ggplot')
@@ -26,15 +36,35 @@ plt.rcParams['font.serif'] = 'DejaVu Sans'
 plt.rcParams['font.monospace'] = 'Inconsolata Medium'
 plt.rcParams['axes.labelweight'] = 'bold'
 
-dataframe = construct_dataframe(stats['Symptoms']).sort_values(
-    'Number of bugs', ascending=True)
-ax = dataframe.plot.barh(width=0.3, x='Symptom', color='grey')
-for p in ax.patches:
-    ax.annotate("{} / 160".format(p.get_width()),
+bugs = jv.java_iter1 + jv.java_iter2 + jv.java_iter3 + \
+    sc.scala_iter1 + sc.scala_iter2 + sc.scala_iter3 + \
+    kt.kotlin_iter1 + kt.kotlin_iter2 + kt.kotlin_iter3 + \
+    gv.groovy_iter1 + gv.groovy_iter2 + gv.groovy_iter3
+
+df, data = construct_dataframe(bugs)
+df = df.groupby(['Language', 'Symptom'])['Number of bugs'].sum().unstack('Language')
+categories = [
+    'Compilation Performance Issue',
+    'Misleading Report',
+    'Unexpected Runtime Behavior',
+    'Internal Compiler Error',
+    'Unexpected Compile-Time Error',
+]
+df = df.reindex(categories)
+
+ax = df.plot.barh(width=0.3, color=['#f6cb7d', '#873e23', '#1e81b0', '#8A0528'],
+                  stacked=True)
+
+sums = []
+for c in reversed(categories):
+    v = sum(data[(lang, c)] for lang in ['Groovy', 'Java', 'Kotlin', 'Scala'])
+    sums.append(v)
+
+for i, p in enumerate(ax.patches[16:20]):
+    ax.annotate("{} / 240".format(int(sums[i])),
                 (p.get_x() + p.get_width(), p.get_y()),
                 xytext=(5, 10), textcoords='offset points')
 ax.set_ylabel('')
 patches, labels = ax.get_legend_handles_labels()
-ax.legend(patches, ('Number of bugs',), loc='lower right')
 plt.savefig("symptoms.pdf", format='pdf', bbox_inches='tight',
             pad_inches=0)
