381301839ea84599275e18b8c30aaa22c7398add
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-04-13 15:15:33 +0300

    Update characteristics script

--- study/categorization/characteristics_fig.py
+++ study/categorization/characteristics_fig.py
@@ -1,10 +1,22 @@
 #! /usr/bin/env python3
+import argparse
+import json
+
 import matplotlib.pylab as plt
 import matplotlib.offsetbox as offsetbox
 import seaborn as sns
 import pandas as pd
 
-from utils import stats
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Generate characteristics figure.')
+    parser.add_argument("data", help="JSON with bugs.")
+    parser.add_argument(
+            "--output",
+            default="characteristics.pdf",
+            help="Filename to save the figure.")
+    return parser.parse_args()
 
 
 def construct_dataframe(data):
@@ -29,42 +41,52 @@ def construct_dataframe(data):
     return dataframes, characteristics
 
 
-plt.style.use('ggplot')
-sns.set(style="whitegrid")
-plt.rcParams['font.family'] = 'DejaVu Sans'
-plt.rcParams['figure.figsize'] = (8, 4.5)
-plt.rcParams['axes.labelsize'] = 8
-plt.rcParams['ytick.labelsize'] = 6.8
-plt.rcParams['xtick.labelsize'] = 6
-plt.rcParams['font.serif'] = 'DejaVu Sans'
-plt.rcParams['font.monospace'] = 'Inconsolata Medium'
-plt.rcParams['axes.labelweight'] = 'bold'
+def plot_fig(dataframes, output):
+    plt.style.use('ggplot')
+    sns.set(style="whitegrid")
+    plt.rcParams['font.family'] = 'DejaVu Sans'
+    plt.rcParams['figure.figsize'] = (8, 4.5)
+    plt.rcParams['axes.labelsize'] = 8
+    plt.rcParams['ytick.labelsize'] = 6.8
+    plt.rcParams['xtick.labelsize'] = 6
+    plt.rcParams['font.serif'] = 'DejaVu Sans'
+    plt.rcParams['font.monospace'] = 'Inconsolata Medium'
+    plt.rcParams['axes.labelweight'] = 'bold'
 
-fig, axs = plt.subplots(nrows=8, sharex=True)
-dataframes, characteristics = construct_dataframe(
-    stats['Characteristics']['Categories'])
+    fig, axs = plt.subplots(nrows=8, sharex=True)
 
-characteristics = sorted(characteristics, key=lambda tup: tup[2])
-print(characteristics[:7])
-print(characteristics[-7:])
+    for i, (key, dataframe) in enumerate(dataframes.items()):
+        dataframe = dataframe.sort_values(
+            'Number of bugs', ascending=True)
+        dataframe.plot.barh(x='Characteristic', y='Number of bugs',
+                            color='grey', ax=axs[i])
+        ob = offsetbox.AnchoredText(key, loc=1,
+                                    prop=dict(color='black', fontsize=7))
+        ob.patch.set(boxstyle='round', color='lightgrey', alpha=1)
+        axs[i].add_artist(ob)
+        axs[i].set_ylabel('')
+        axs[i].set_xlabel('Number of bugs (%)')
+        axs[i].get_legend().remove()
+        axs[i].set_xlim([0, 70])
+        for line in axs[i].get_xgridlines():
+            line.set_linewidth(0.3)
+        for line in axs[i].get_ygridlines():
+            line.set_linewidth(0.3)
+        [i.set_linewidth(0.3) for i in axs[i].spines.values()]
+    plt.savefig(output, format='pdf', bbox_inches='tight',
+                pad_inches=0)
 
-for i, (key, dataframe) in enumerate(dataframes.items()):
-    dataframe = dataframe.sort_values(
-        'Number of bugs', ascending=True)
-    dataframe.plot.barh(x='Characteristic', y='Number of bugs',
-                        color='grey', ax=axs[i])
-    ob = offsetbox.AnchoredText(key, loc=1,
-                                prop=dict(color='black', fontsize=7))
-    ob.patch.set(boxstyle='round', color='lightgrey', alpha=1)
-    axs[i].add_artist(ob)
-    axs[i].set_ylabel('')
-    axs[i].set_xlabel('Number of bugs (%)')
-    axs[i].get_legend().remove()
-    axs[i].set_xlim([0, 70])
-    for line in axs[i].get_xgridlines():
-        line.set_linewidth(0.3)
-    for line in axs[i].get_ygridlines():
-        line.set_linewidth(0.3)
-    [i.set_linewidth(0.3) for i in axs[i].spines.values()]
-plt.savefig("characteristics.pdf", format='pdf', bbox_inches='tight',
-            pad_inches=0)
+
+def main():
+    args = get_args()
+    with open(args.data, 'r') as f:
+        json_data = json.load(f)
+    dataframes, characteristics = construct_dataframe(json_data['Categories'])
+    characteristics = sorted(characteristics, key=lambda tup: tup[2])
+    print(characteristics[:7])
+    print(characteristics[-7:])
+    plot_fig(dataframes, args.output)
+
+
+if __name__ == "__main__":
+    main()
--- study/categorization/serialize.py
+++ study/categorization/serialize.py
@@ -2,16 +2,23 @@
 import argparse
 import json
 
+from collections import defaultdict
+
 import kotlin as kt
 import java as jv
 import scala as sc
 import groovy as gv
+
+from characteristics import CharacteristicCategory
 
 
 def get_args():
     parser = argparse.ArgumentParser(
         description='Serialize bugs.')
     parser.add_argument("output", help="File to save the bugs.")
+    parser.add_argument(
+        "characteristics",
+        help="File to save bug characteristics distribution.")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -21,15 +28,47 @@ if __name__ == "__main__":
         kt.kotlin_iter1 + kt.kotlin_iter2 + kt.kotlin_iter3 + kt.kotlin_iter4 + \
         gv.groovy_iter1 + gv.groovy_iter2 + gv.groovy_iter3 + gv.groovy_iter4
     res = {}
+    char_stats = {
+            "Categories": defaultdict(
+                lambda: {"total": 0,
+                         "subcategories": defaultdict(
+                             lambda: {"total": 0,
+                                      "subcategories": defaultdict(
+                                          lambda: 0)})}),
+            "Types": defaultdict(lambda: 0),
+            "Commons": {"True": 0, "False": 0}}
     for b in bugs:
         # bug id
         bug_id = b.bug_id.split('.')[-1] if '.' in b.bug_id else b.bug_id
         # characteristics
         chars = {"characteristics": set(), "categories": set()}
+        visited = set()
         for c in b.characteristics:
             chars["characteristics"].add(c.name)
             if c.category is not None:
                 chars["categories"].add(c.category.name)
+            # Characteristics distribution
+            if isinstance(c.category, CharacteristicCategory):
+                cat = char_stats["Categories"][c.category.name]
+                if c.category.name not in visited:
+                    cat["total"] += 1
+                    visited.add(c.category.name)
+                cat["subcategories"][c.name]["total"] += 1
+                cat["subcategories"][c.name]["is_common"] = c.is_common
+            else:
+                cat = char_stats["Categories"][c.category.category.name]
+                if c.category.category.name not in visited:
+                    cat["total"] += 1
+                    visited.add(c.category.category.name)
+                cat["subcategories"][c.category.name]["total"] += 1
+                cat["subcategories"][c.category.name]["is_common"] = c.is_common
+                subs = cat["subcategories"][c.category.name]["subcategories"]
+                subs["is_common"] = c.is_common
+                subs[c.name] += 1
+            char_stats["Commons"][str(c.is_common)] += 1
+            if c.characteristic_type:
+                char_stats["Types"][c.characteristic_type.name] += 1
+
         # Convert sets to lists
         chars["characteristics"] = list(chars["characteristics"])
         chars["categories"] = list(chars["categories"])
@@ -55,3 +94,5 @@ if __name__ == "__main__":
         }
     with open(args.output, 'w') as f:
         json.dump(res, f, indent=4)
+    with open(args.characteristics, 'w') as f:
+        json.dump(char_stats, f, indent=4)
