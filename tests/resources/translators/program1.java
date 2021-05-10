337304aa775ad0028ed66bfedef8acb76223b359
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-05-10 12:09:59 +0300

    Add two translator tests for Java

---
+++ tests/resources/translators/program1.java
@@ -0,0 +1,24 @@
+class Main {
+    public static final void main() {
+        B b = new B("b");
+        A ba = new B("ba");
+    }
+}
+
+class A {
+    public final String a;
+
+    public A(String a) {
+        this.a = a;
+    }
+
+}
+
+class B extends A {
+    public final String a;
+    public B(String a) {
+        this.a = a;
+    }
+}
+
+final class C {}
---
+++ tests/resources/translators/program2.java
@@ -0,0 +1,28 @@
+class Main {
+    static final String z = "z";
+    static final String y = "y";
+    static final String bar(String y) {
+        return (Main.z + y);
+    }
+    static final A buz() {
+        return new A("a");
+    }
+    public static final void main() {
+        A a = new A("a");
+        a.foo();
+        Main.buz().foo();
+    }
+}
+
+class A {
+    public final String a;
+
+    public A(String a) {
+        this.a = a;
+    }
+
+    void foo() {
+        println(Main.bar(a));
+        println(Main.bar(Main.z));
+    }
+}
