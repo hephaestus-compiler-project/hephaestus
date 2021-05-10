906bdeb452dc1f36681eecf85d29a0f03f7aea99
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-05-10 17:42:30 +0300

    Use get_type_name

--- src/translators/utils.py
+++ src/translators/utils.py
@@ -1,4 +1,4 @@
-from src.ir import kotlin_types as kt, groovy_types as gt
+from src.ir import kotlin_types as kt, groovy_types as gt, java_types as jt
 
 
 def get_type_name(t):
@@ -9,6 +9,8 @@ def get_type_name(t):
         return "{}[]".format(get_type_name(t.type_args[0]))
     elif isinstance(t_constructor, kt.SpecializedArrayType):
         return "{}Array".format(get_type_name(t.type_args[0]))
+    elif isinstance(t_constructor, jt.ArrayType):
+        return "{}[]".format(get_type_name(t.type_args[0]))
     else:
         return "{}<{}>".format(t.name, ", ".join([get_type_name(ta)
                                                   for ta in t.type_args]))
