e64a840924b5cb9cf581fb5f194104531e73d59d
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-05-12 13:43:53 +0300

    Handle void types

--- src/translators/utils.py
+++ src/translators/utils.py
@@ -1,16 +1,17 @@
 from src.ir import kotlin_types as kt, groovy_types as gt, java_types as jt
 
 
-def get_type_name(t):
+def get_type_name(t, get_boxed_void=False):
     t_constructor = getattr(t, 't_constructor', None)
     if not t_constructor:
+        if get_boxed_void and isinstance(t, jt.VoidType):
+            return "Void"
         return t.get_name()
     if isinstance(t_constructor, gt.ArrayType):
         return "{}[]".format(get_type_name(t.type_args[0]))
-    elif isinstance(t_constructor, kt.SpecializedArrayType):
+    if isinstance(t_constructor, kt.SpecializedArrayType):
         return "{}Array".format(get_type_name(t.type_args[0]))
-    elif isinstance(t_constructor, jt.ArrayType):
+    if isinstance(t_constructor, jt.ArrayType):
         return "{}[]".format(get_type_name(t.type_args[0]))
-    else:
-        return "{}<{}>".format(t.name, ", ".join([get_type_name(ta)
-                                                  for ta in t.type_args]))
+    return "{}<{}>".format(t.name, ", ".join([get_type_name(ta, True)
+                                              for ta in t.type_args]))
