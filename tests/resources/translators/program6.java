7e001032be4d9223f61195a06bdf2e7029644da7
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-05-19 15:46:07 +0300

    Add test for conditional block

---
+++ tests/resources/translators/program6.java
@@ -0,0 +1,33 @@
+class Main {
+    public static final void main() {
+        Object b = new B("b");
+        A ba = new B("ba");
+        A bb = ((b instanceof A b_is) ?
+                 b_is :
+                  ((Function0<A>)  (() -> {
+                    A bc = new B("bc");
+                    return bc;
+                 })).apply());
+    }
+}
+
+interface Function0<A1> {
+  public A1 apply();
+}
+
+class A {
+    public final String a;
+
+    public A(String a) {
+        this.a = a;
+    }
+
+}
+
+class B extends A {
+    public final String a;
+    public B(String a) {
+        super("b");
+        this.a = a;
+    }
+}
