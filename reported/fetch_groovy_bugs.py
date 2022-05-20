3c9d7f06209ff3b7c25ff2a965bcfbfe5bdbb501
Thodoris Sotiropoulos <theosotr@aueb.gr>
2022-05-20 14:49:50 +0300

    Fix regex for retrieving test cases from bug reports

--- reported/fetch_groovy_bugs.py
+++ reported/fetch_groovy_bugs.py
@@ -43,9 +43,9 @@ SCHEMA = {
 
 
 def get_code_fragments(text):
-    matches = re.findall('(?:{code:(?:java|groovy)})(.*?)(?:{code})', text, flags=re.I | re.DOTALL)
+    matches = re.findall('(?:{code:?(?:java|groovy)?})(.*?)(?:{code})', text, flags=re.I | re.DOTALL)
     res = []
-    for m in matches:
+    for m in matches[:1]:
         res.append([x.replace('\t', '  ') for x in m.splitlines() if x.strip() != ''])
         res = [r for r in res if len(r) > 0]
     return res
