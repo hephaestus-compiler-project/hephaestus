ffe988011b2b695a9505d7346d4e025d84e270d6
Alex Papadopoulos <papadopoulos.alexios@gmail.com>
2022-07-26 16:42:49 +0300

    add regex for tsc errors

--- src/compilers/typescript.py
+++ src/compilers/typescript.py
@@ -5,7 +5,7 @@ from src.compilers.base import BaseCompiler
 
 class TypeScriptCompiler(BaseCompiler):
 
-    ERROR_REGEX = re.compile(".*")
+    ERROR_REGEX = re.compile(r'([A-Za-z0-9_\-\/]+\.ts):\d+:\d+ - error (TS\d+): (.+)')
 
     CRASH_REGEX = re.compile("123")
 
@@ -21,17 +21,17 @@ class TypeScriptCompiler(BaseCompiler):
         return ['tsc --target es2020', self.input_name]
 
     def analyze_compiler_output(self, output):
-        return defaultdict(list)
-        """ self.crashed = None
+        self.crashed = None
         failed = defaultdict(list)
         matches = re.findall(self.ERROR_REGEX, output)
         for match in matches:
             filename = match[0]
-            error_msg = match[1]
+            error_msg = match[2]
             failed[filename].append(error_msg)
+        
 
         crash_match = re.search(self.CRASH_REGEX, output)
         if crash_match and not matches:
             self.crash_msg = output
             return None
-        return failed """
+        return failed
