00a68d8cd4d9c34a6a11fa7d6914613abe539be0
Thodoris Sotiropoulos <theosotr@aueb.gr>
2023-06-16 15:19:31 +0200

    Add support for more Scala types

--- src/ir/scala_types.py
+++ src/ir/scala_types.py
@@ -17,25 +17,28 @@ class ScalaBuiltinFactory(bt.BuiltinFactory):
     def get_any_type(self):
         return AnyType()
 
+    def get_anyref_type(self):
+        return AnyRefType()
+
     def get_number_type(self):
         return NumberType()
 
-    def get_integer_type(self):
+    def get_integer_type(self, primitive=False):
         return IntegerType()
 
-    def get_byte_type(self):
+    def get_byte_type(self, primitive=False):
         return ByteType()
 
-    def get_short_type(self):
+    def get_short_type(self, primitive=False):
         return ShortType()
 
-    def get_long_type(self):
+    def get_long_type(self, primitive=False):
         return LongType()
 
-    def get_float_type(self):
+    def get_float_type(self, primitive=False):
         return FloatType()
 
-    def get_double_type(self):
+    def get_double_type(self, primitive=False):
         return DoubleType()
 
     def get_big_decimal_type(self):
@@ -44,10 +47,10 @@ class ScalaBuiltinFactory(bt.BuiltinFactory):
     def get_big_integer_type(self):
         return IntegerType()
 
-    def get_boolean_type(self):
+    def get_boolean_type(self, primitive=False):
         return BooleanType()
 
-    def get_char_type(self):
+    def get_char_type(self, primitive=False):
         return CharType()
 
     def get_string_type(self):
@@ -86,6 +89,12 @@ class AnyType(ScalaBuiltin):
         return bt.Any
 
 
+class AnyRefType(AnyType):
+    def __init__(self, name="AnyRef"):
+        super().__init__(name)
+        self.supertypes.append(AnyType())
+
+
 class NothingType(ScalaBuiltin):
     def __init__(self, name="Nothing"):
         super().__init__(name)
@@ -106,10 +115,10 @@ class UnitType(AnyType):
         return bt.Void
 
 
-class NumberType(AnyType):
+class NumberType(AnyRefType):
     def __init__(self, name="Number"):
         super().__init__(name)
-        self.supertypes.append(AnyType())
+        self.supertypes.append(AnyRefType())
 
     def get_builtin_type(self):
         return bt.Number
@@ -178,10 +187,10 @@ class CharType(AnyType):
         return bt.Char
 
 
-class StringType(AnyType):
+class StringType(AnyRefType):
     def __init__(self, name="String"):
         super().__init__(name)
-        self.supertypes.append(AnyType())
+        self.supertypes.append(AnyRefType())
 
     def get_builtin_type(self):
         return bt.String
@@ -196,20 +205,20 @@ class BooleanType(AnyType):
         return bt.Boolean
 
 
-class ArrayType(tp.TypeConstructor, AnyType):
+class ArrayType(tp.TypeConstructor, AnyRefType):
     def __init__(self, name="Array"):
         # In Scala, arrays are invariant.
         super().__init__(name, [tp.TypeParameter("T")])
-        self.supertypes.append(AnyType())
+        self.supertypes.append(AnyRefType())
 
 
-class SeqType(tp.TypeConstructor, AnyType):
+class SeqType(tp.TypeConstructor, AnyRefType):
     def __init__(self, name="Seq"):
         super().__init__(name, [tp.TypeParameter("T", variance=tp.Covariant)])
-        self.supertypes.append(AnyType())
+        self.supertypes.append(AnyRefType())
 
 
-class FunctionType(tp.TypeConstructor, AnyType):
+class FunctionType(tp.TypeConstructor, AnyRefType):
     def __init__(self, nr_type_parameters: int):
         name = "Function" + str(nr_type_parameters)
         # We can have decl-variance in Scala
@@ -219,7 +228,20 @@ class FunctionType(tp.TypeConstructor, AnyType):
         ] + [tp.TypeParameter("R", tp.Covariant)]
         self.nr_type_parameters = nr_type_parameters
         super().__init__(name, type_parameters)
-        self.supertypes.append(AnyType())
+        self.supertypes.append(AnyRefType())
+
+
+class TupleType(tp.TypeConstructor, AnyRefType):
+    def __init__(self, n_type_parameters: int):
+        name = "Tuple" + str(n_type_parameters)
+        # We can have decl-variance in Scala
+        type_parameters = [
+            tp.TypeParameter("A" + str(i + 1), tp.Contravariant)
+            for i in range(n_type_parameters)
+        ]
+        self.nr_type_parameters = n_type_parameters
+        super().__init__(name, type_parameters)
+        self.supertypes.append(AnyRefType())
 
 
 Any = AnyType()
@@ -237,6 +259,7 @@ String = StringType()
 Boolean = BooleanType()
 Array = ArrayType()
 Seq = SeqType()
+AnyRef = AnyRefType()
 
 NonNothingTypes = [Any, Number, Integer, Short, Long, Byte, Float,
                    Double, Char, String, Boolean, Array, Seq]
